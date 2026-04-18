d0354aaeedf69a205dfeaf6de9fc5ffb22921105
Oxide 1-6 <yes.par781@gmail.com>
2026-04-18 13:14:00 +0530

    [Perf]: Drop unused providers and mount hidden terminal

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { MotionConfig } from "framer-motion";
 import AmbientBackground from "./components/AmbientBackground";
@@ -10,28 +6,20 @@ import Terminal from "./components/Terminal";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
-const queryClient = new QueryClient();
-
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <MotionConfig reducedMotion="user">
-      <TooltipProvider>
-        <AmbientBackground />
-        <Toaster />
-        <Sonner />
-        <TransmissionProvider>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Index />} />
-              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </BrowserRouter>
-          <Terminal />
-        </TransmissionProvider>
-      </TooltipProvider>
-    </MotionConfig>
-  </QueryClientProvider>
+  <MotionConfig reducedMotion="user">
+    <AmbientBackground />
+    <TransmissionProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+      <Terminal />
+    </TransmissionProvider>
+  </MotionConfig>
 );
 
 export default App;
